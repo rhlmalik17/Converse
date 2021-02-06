e6b22a411a514a4952fb5fc23c5fd2273befa6f3
rhlmalik17 <rhlmalik17@gmail.com>
2021-02-06 22:14:31 +0530

    Authentication Screens

--- src/app/Authentication/SignIn/SignIn.tsx
+++ src/app/Authentication/SignIn/SignIn.tsx
@@ -1,9 +1,20 @@
 import './SignIn.css'
+import '../Authentication.scss'
 
 export default function SignIn() {
     return (
-        <div>
-            Sign In
+        <div className="sign__in__container">
+            <div className="text-field-container email__field">
+                <input placeholder="Enter Email" type="email" />
+            </div>
+
+            <div className="text-field-container password__field">
+                <input placeholder="Enter Password" type="password" />
+            </div>
+            
+            <div className="sign__in__options d-flex justify-content-center">
+                <button className="primary-btn auth-btn"> Sign In </button>
+            </div>
         </div>
     )
 }
--- src/app/Authentication/SignUp/SignUp.tsx
+++ src/app/Authentication/SignUp/SignUp.tsx
@@ -3,8 +3,26 @@ import './SignUp.css'
 
 function SignUp() {
     return (
-        <div>
-            Sign Up
+        <div className="sign__up__container">
+            <div className="text-field-container name__field">
+                <input placeholder="First Name" type="text" />
+            </div>
+
+            <div className="text-field-container name__field">
+                <input placeholder="Last Name" type="text" />
+            </div>
+
+            <div className="text-field-container email__field">
+                <input placeholder="Enter Email" type="email" />
+            </div>
+
+            <div className="text-field-container password__field">
+                <input placeholder="Enter Password" type="password" />
+            </div>
+            
+            <div className="sign__up__options d-flex justify-content-center">
+                <button className="primary-btn auth-btn"> Sign Up  </button>
+            </div>
         </div>
     )
 }
